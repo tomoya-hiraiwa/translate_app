709e0be0c5e2ac0075e2be7ee4d931f0f17cf82b
tomoya-hiraiwa <tomoya5568.n.tfamily7667@gmail.com>
2023-12-21 11:16:35 +0900

    complete http connection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,8 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
--- app/src/main/java/edu/wsc2022/a01/mrtranslate/MainActivity.kt
+++ app/src/main/java/edu/wsc2022/a01/mrtranslate/MainActivity.kt
@@ -2,13 +2,39 @@ package edu.wsc2022.a01.mrtranslate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
 import edu.wsc2022.a01.mrtranslate.databinding.ActivityMainBinding
-
+const val Token = "enter your token"
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val tab = binding.tab
+        tab.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when(tab?.position){
+                    0 -> changeFragment(TranslateFragment())
+                    1 -> changeFragment(LibraryFragment())
+                    2 -> changeFragment(SettingFragment())
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+
+        })
+    }
+    private fun changeFragment(fm: Fragment){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerView,fm)
+            .commit()
     }
 }
